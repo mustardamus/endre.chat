22eb77c0983f390bcfd51a729e657deaf5f5d31f
Mustardamus <contact@mustardamus.com>
2023-03-16 19:55:18 +0100

    downgraded unocss so hmr with svelte works

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-8">Welcome to SvelteKit</h1>
+<h1 class="text-12">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
