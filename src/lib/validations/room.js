2b4874ad64358cd2a700bee93d3c947f8c03e62b
Mustardamus <contact@mustardamus.com>
2023-04-11 11:24:11 +0200

    tighter validations for user, room and message

--- src/lib/validations/room.js
+++ src/lib/validations/room.js
@@ -1,8 +1,25 @@
 import { create, enforce, test } from "vest";
+import { isAlphanumeric } from "validator";
 
 export const createRoom = create((data = {}) => {
   test("roomName", "Room name is required", () => {
     enforce(data.roomName).isNotEmpty();
+  });
+
+  test("roomName", "Room name must have at least 3 characters", () => {
+    enforce(data.roomName?.length >= 3).isTruthy();
+  });
+
+  test("roomName", "Room name can not be longer than 9 characters", () => {
+    enforce(data.roomName?.length <= 9).isTruthy();
+  });
+
+  test("roomName", "Room name can only have letters and numbers", () => {
+    enforce(isAlphanumeric(data.roomName)).isTruthy();
+  });
+
+  test("roomName", "Room name can not be 'create'", () => {
+    enforce(data.roomName !== "create").isTruthy();
   });
 
   test("filterId", "Filter must be set", () => {
--- src/lib/validations/user.js
+++ src/lib/validations/user.js
@@ -1,7 +1,20 @@
 import { create, enforce, test } from "vest";
+import { isAlphanumeric } from "validator";
 
 export default create((data = {}) => {
   test("userName", "Username is required", () => {
     enforce(data.userName).isNotEmpty();
+  });
+
+  test("userName", "Username must have at least 3 characters", () => {
+    enforce(data.userName?.length >= 3).isTruthy();
+  });
+
+  test("userName", "Username can not be longer than 9 characters", () => {
+    enforce(data.userName?.length <= 9).isTruthy();
+  });
+
+  test("userName", "Username can only have letters and numbers", () => {
+    enforce(isAlphanumeric(data.userName)).isTruthy();
   });
 });
