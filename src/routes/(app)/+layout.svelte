cc5a91c6dfa70775d7d1f21c69d756c1d638079b
Mustardamus <contact@mustardamus.com>
2023-04-17 12:29:23 +0200

    fixed full height on mobile, readded immutable dependency

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,10 +8,18 @@
   export let data;
 </script>
 
-<div class="h-screen flex flex-col overflow-hidden">
+<div class="h-full flex flex-col overflow-hidden">
   <NavBar height="2rem" brand="endre.chat" currentUser={data.currentUser} />
 
   <div class="flex-grow overflow-hidden">
     <slot />
   </div>
 </div>
+
+<style>
+  :global(html, body, #app) {
+    height: calc(
+      100vh - calc(100vh - 100%)
+    ); /* https://stackoverflow.com/a/60735432 */
+  }
+</style>
